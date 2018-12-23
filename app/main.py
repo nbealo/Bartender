1a8fbe41c85e8688f9819b946987fb4038a3c511
Chris Hanna <cdhannaphone@gmail.com>
2018-12-23 16:43:59 -0500

    adds basic qt

--- app/main.py
+++ app/main.py
@@ -1,19 +1,32 @@
-# from PyQt5.QtWidgets import QApplication, QLabel
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
 
+from lib.qt_style_helper import Styles
 from models.system_spec_model import SystemSpecModel
 from services.persistence_service import PersistenceService
 
 def main():
     config = PersistenceService.load_config('./app/config')
-    print(config)
-    # spec = PersistenceService.load_system_spec('./app/config/system_spec.json')
-    # drinks = PersistenceService.load_drinks('./app/config/drinks')
-    # print(drinks)
-    #print(spec.__dict__)
-    # app = QApplication([])
-    # label = QLabel('Hello World!')
-    # label.show()
-    # app.exec_()
+    spec = config[0]
+    drinks = config[1]
 
+    app = QApplication(sys.argv)
+    window = QWidget()
+    window.resize(300,400)
+    window.move(0,0)
+    window.setWindowTitle('BarBot')
 
+    l1 = QLabel('Scale Pin Number')
+    nm = QLineEdit()
+    nm.setText(spec.scale_pin_number)
+
+    fbox = QFormLayout()
+    fbox.addRow(l1, nm)
+
+    window.setLayout(fbox)
+    window.show()
+    sys.exit(app.exec_())
+    
 main()
