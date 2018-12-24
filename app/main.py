4e479467d3307d1b0f4276a71b75596a27c69833
nick bealo <nbealo@comcast.net>
2018-12-24 16:36:23 -0500

    adds web server

--- app/main.py
+++ app/main.py
@@ -6,27 +6,39 @@ from PyQt5.QtCore import *
 from lib.qt_style_helper import Styles
 from models.system_spec_model import SystemSpecModel
 from services.persistence_service import PersistenceService
+from flask import Flask
+app = Flask(__name__)
 
-def main():
-    config = PersistenceService.load_config('./app/config')
-    spec = config[0]
-    drinks = config[1]
+# def main():
+#     config = PersistenceService.load_config('./app/config')
+#     spec = config[0]
+#     drinks = config[1]
 
-    app = QApplication(sys.argv)
-    window = QWidget()
-    window.resize(300,400)
-    window.move(0,0)
-    window.setWindowTitle('BarBot')
+#     app = QApplication(sys.argv)
+#     window = QWidget()
+#     window.resize(300,400)
+#     window.move(0,0)
+#     window.setWindowTitle('BarBot')
 
-    l1 = QLabel('Scale Pin Number')
-    nm = QLineEdit()
-    nm.setText(spec.scale_pin_number)
+#     l1 = QLabel('Scale Pin Number')
+#     nm = QLineEdit()
+#     nm.setText(spec.scale_pin_number)
 
-    fbox = QFormLayout()
-    fbox.addRow(l1, nm)
+#     fbox = QFormLayout()
+#     fbox.addRow(l1, nm)
 
-    window.setLayout(fbox)
-    window.show()
-    sys.exit(app.exec_())
+#     window.setLayout(fbox)
+#     window.show()
+#     sys.exit(app.exec_())
     
-main()
+# main()
+
+@app.route("/")
+def main():
+     return "hello"
+
+@app.route("/x")
+def main2():
+     return "World"
+    
+app.run()
