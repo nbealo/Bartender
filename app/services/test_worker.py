e85e3ebad757c0d54e75d213f1c5b2ff3a0bcf0a
Chris Hanna <cdhannaphone@gmail.com>
2019-03-14 20:06:09 -0400

    huh

--- app/services/test_worker.py
+++ app/services/test_worker.py
@@ -32,17 +32,23 @@ class WorkerThread(threading.Thread):
         self.hx = HX711(5, 6, 64)
         self.hx.set_reading_format("MSB", "MSB")
 
+
+        # at 128 gain.
         # 1 kg -> 458591 -> 460
         # 2.75 kg -> 1146261 -> 416
 
-        self.reference_unit = 416
-        # self.hx.set_reference_unit(self.reference_unit)
+
+        # at 64 gain
+        # 2.75 kg -> 591915 -> 215
+
+        self.reference_unit = 215
+        self.hx.set_reference_unit(self.reference_unit)
         self.hx.reset()
         while not self.stoprequest.isSet():
 
             val = self.hx.read_long()
             weight = 0
-            # weight = val / self.reference_unit
+            weight = val / self.reference_unit
             print(str(val) + ', ' + str(weight))
 
             self.blackboard.set('weight', weight)
